a1a7e54a67ee88f240ecb0be47eb7b1c355f653e
Levi-24 <leventemajor36@gmail.com>
2024-09-03 11:16:41 +0200

    Add project files.

---
+++ LabSim.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Labirintus
+{
+    internal class LabSim
+    {
+        private List<string> adatsorok;
+        private char[,] lab;
+
+        public bool KeresesKesz { get; set; }
+        public int KijaratOszlopIndex { get; }
+        public int KijaratSorIndex { get; }
+        public bool NincsMegoldas { get; set; }
+        public int OszlopokSzama { get; }
+        public int SorokSzama { get; }
+
+        public void AdatsorokBeolvasasa(string forras)
+        {
+            using StreamReader sr = new(@$"..\..\..\src\{forras}");
+        }
+
+        public LabSim(string fileName)
+        {
+            AdatsorokBeolvasasa(fileName);
+        }
+    }
+}
